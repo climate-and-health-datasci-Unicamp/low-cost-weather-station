b9062d9c1dd5c14712f953cfd9d1eb459a990423
unknown <guiii88@outlook.com>
2020-09-04 20:51:52 -0300

    Put code to register log file with the station information

--- src/temphumsensor/temphumsensor.ino
+++ src/temphumsensor/temphumsensor.ino
@@ -1,55 +1,53 @@
 #include <Wire.h>
+#include <EEPROM.h>
 #include <RtcDS3231.h>
 #include <Adafruit_HTU21DF.h>
-#include <SD.h>
-#include <EEPROM.h>
-#include <ESP8266WiFi.h>
+
+#include <SPI.h>
+//#include <SD.h>
+#include "SdFat.h"
+SdFat SD;
+
+#define SD_CS_PIN D8
+File dataFile;
+File logFile;
 
 Adafruit_HTU21DF htu; //temperature and humidity sensor object
 RtcDS3231<TwoWire> Rtc(Wire); //RTC object
 
-//pino ligado ao CS do módulo SD Card
-#define CS_PIN  D8
 #define sec 1000
-#define VCC_PIN D3
 
+//Macros for date
+#define year 2020
+#define month 8
+#define dayOfMonth 3
+#define hour 20
+#define minute 0
+#define second 0
+    
 void setup () 
 {   
-    pinMode(VCC_PIN, OUTPUT);
-    digitalWrite(VCC_PIN, HIGH); //turn off power suplly for sensors, rtc and sd module.
     
-    htu.begin(); // Inicia sensor de temperatura e umidade
-    //Serial.begin(115200); // Inicia porta serial
+    htu.begin(); // Inicia sensor de temperatura e umidade                                  
+    Serial.begin(115200); // Inicia porta serial
     Rtc.Begin(); // Inicia RTC
     EEPROM.begin(4);//Inicia a EEPROM com tamanho de 4 Bytes (minimo).
-    WiFi.mode( WIFI_OFF );
-    WiFi.forceSleepBegin();
     
     // verifica se o cartão SD está presente e se pode ser inicializado
-    if (!SD.begin(CS_PIN)) 
-    {
-        return;
-    }
+    if (!SD.begin(SD_CS_PIN)) return;
     
     RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
-    if (!Rtc.IsDateTimeValid()) 
-    {
-        Rtc.SetDateTime(compiled);
-    }
-    
-    if (!Rtc.GetIsRunning())
-    {
-        Rtc.SetIsRunning(true);
-    }
+    if (!Rtc.IsDateTimeValid()) Rtc.SetDateTime(compiled);
+    if (!Rtc.GetIsRunning())  Rtc.SetIsRunning(true);
 
     Rtc.Enable32kHzPin(false);
     Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeAlarmOne);
     
-    int on = EEPROM.read(0);
-    if (on)
+    int ison = EEPROM.read(0);
+    if (ison)
     {
         RtcDateTime now = Rtc.GetDateTime();
-        RtcDateTime alarmTime = now+1800; // it adds 30 min
+        RtcDateTime alarmTime = now+30;//3600; // it adds 1h
         DS3231AlarmOne alarm1(
                 alarmTime.Day(),
                 alarmTime.Hour(),
@@ -57,23 +55,70 @@ void setup ()
                 alarmTime.Second(),
                 DS3231AlarmOneControl_MinutesSecondsMatch);
         Rtc.SetAlarmOne(alarm1);
+
+        logFile = SD.open("log.txt", FILE_WRITE);
+        // se o arquivo foi aberto corretamente, escreve os dados nele
+        if (logFile) 
+        {
+            logFile.seek(logFile.size() - 10);
+            logFile.print(now.Year(), DEC);
+            logFile.print('-');
+            if (now.Month() < 10) logFile.print("0");
+            logFile.print(now.Month(), DEC);
+            logFile.print('-');
+            if (now.Day() < 10) logFile.print("0");
+            logFile.print(now.Day(), DEC);
+
+            
+            //fecha o arquivo após usá-lo
+            logFile.close();
+        }
+        
     }
     else
     {
         delay(5*sec);
+
+        logFile = SD.open("log.txt", FILE_WRITE);
+        // se o arquivo foi aberto corretamente, escreve os dados nele
+        if (logFile) 
+        {
+            
+            logFile.println("ID da Estacao: 1");
+            logFile.println("Nome da Estacao: APTO Vila Prost Souza");
+            logFile.println("Coordenadas Geograficas (Lat, Lng): -22.899874, -47.093644");
+            logFile.println("Elevacao (em metros): 695");
+            logFile.print("Data de Inicio: ");
+
+            logFile.print(year, DEC);
+            logFile.print('-');
+            if (month < 10) logFile.print("0");
+            logFile.print(month, DEC);
+            logFile.print('-');
+            if (dayOfMonth < 10) logFile.print("0");
+            logFile.println(dayOfMonth, DEC);
+
+            logFile.print("Data de Encerramento: ");
+            logFile.print(year, DEC);
+            logFile.print('-');
+            if (month < 10) logFile.print("0");
+            logFile.print(month, DEC);
+            logFile.print('-');
+            if (dayOfMonth < 10) logFile.print("0");
+            logFile.print(dayOfMonth, DEC);
+
+            
+            //fecha o arquivo após usá-lo
+            logFile.close();
+        }
+        
         EEPROM.write(0, 1);//Esp8266 on
         EEPROM.commit();//Salva o dado na EEPROM.
 
-        int year = 2020;
-        int month = 8;
-        int dayOfMonth = 3;
-        int hour = 20;
-        int minute = 0;
-        int second = 0;
         const RtcDateTime* now = new RtcDateTime(year, month, dayOfMonth, hour, minute, second);
         Rtc.SetDateTime(*now);
     
-        RtcDateTime alarmTime = *now+1800; // it adds 30 min
+        RtcDateTime alarmTime = *now+30;//3600; // it adds 1h
         DS3231AlarmOne alarm1(
                 alarmTime.Day(),
                 alarmTime.Hour(),
@@ -92,7 +137,7 @@ void loop ()
     float temp = htu.readTemperature(), hum = htu.readHumidity();
     RtcDateTime now = Rtc.GetDateTime();
 
-    File dataFile = SD.open("weather_station.csv", FILE_WRITE);
+    dataFile = SD.open("weather_station.csv", FILE_WRITE);
     // se o arquivo foi aberto corretamente, escreve os dados nele
     if (dataFile) 
     {
@@ -128,6 +173,5 @@ void loop ()
         dataFile.close();
     }
     
-    digitalWrite(VCC_PIN, LOW); // turn off power suplly for sensors, rtc and sd module. RTC will still work on battery.
-    ESP.deepSleep(0, WAKE_RF_DISABLED); //Put esp8266 to sleep for an undefined time
+    ESP.deepSleep(0, WAKE_RF_DEFAULT); //Put esp8266 to sleep for an undefined time
 }
